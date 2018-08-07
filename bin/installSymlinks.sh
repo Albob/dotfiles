ad1e4d6e12688bf1ec13f34f734e15e506fb6511
Albob <alexis.bauchu@gmail.com>
2018-08-07 14:56:28 +0200

    Bin: add a script to help install all useful symlinks

---
+++ bin/installSymlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/bin
+ln -s "/Applications/p4merge.app/Contents/Resources/launchp4merge" ~/bin/p4merge
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+ln -s "/Applications/Visual Studio Code.app/Contents/MacOS/Electron" ~/bin/vscode
